2d8462400278e7054bb8cb60bdfd21deb152825a
Shannon Beach <shannonbeach@Shannons-MBP-2.attlocal.net>
2017-10-08 16:28:15 -0400

    added posion, star, and point values to game

--- game.js
+++ game.js
@@ -15,13 +15,34 @@ var winningScore = 100;
 // add collectable items to the game
 function addItems() {
   items = game.add.physicsGroup();
-  createItem(375, 300, 'coin');
+  createItem(375, 400, 'coin');
+  createItem(575, 500, 'coin');
+  createItem(225, 500, 'coin');
+  createItem(100, 250, 'coin');
+  createItem(575, 150, 'coin');
+  createItem(525, 300, 'coin');
+  createItem(650, 250, 'coin');
+  createItem(225, 200, 'coin');
+  createItem(375, 100, 'poison');
+  createItem(370,500,'poison');
+  createItem(100, 375, 'poison');
+  createItem(125, 50, 'star');
 }
 
 // add platforms to the game
 function addPlatforms() {
   platforms = game.add.physicsGroup();
-  platforms.create(450, 150, 'platform');
+  platforms.create(450, 550, 'platform');
+  platforms.create(100, 550, 'platform');
+  platforms.create(300, 450, 'platform');
+  platforms.create(250, 150, 'platform');
+  platforms.create(50, 300, 'platform');
+  platforms.create(150, 250, 'platform');
+  platforms.create(650, 300, 'platform');
+  platforms.create(550, 200, 'platform2');
+  platforms.create(300, 450, 'platform2');
+  platforms.create(400, 350, 'platform2');
+  platforms.create(100, 100, 'platform2');
   platforms.setAll('body.immovable', true);
 }
 
@@ -43,7 +64,14 @@ function createBadge() {
 // when the player collects an item on the screen
 function itemHandler(player, item) {
   item.kill();
-  currentScore = currentScore + 10;
+  if(item.key === 'coin'){
+    currentScore = currentScore + 10;
+  }else if(item.key === 'star'){
+    currentScore = currentScore + 15;
+  }else if(item.key === 'poison'){
+    currentScore = currentScore - 10;
+  }
+  
   if (currentScore === winningScore) {
       createBadge();
   }
@@ -65,11 +93,14 @@ window.onload = function () {
     
     //Load images
     game.load.image('platform', 'platform_1.png');
+    game.load.image('platform2', 'platform_2.png');
     
     //Load spritesheets
     game.load.spritesheet('player', 'chalkers.png', 48, 62);
     game.load.spritesheet('coin', 'coin.png', 36, 44);
     game.load.spritesheet('badge', 'badge.png', 42, 54);
+    game.load.spritesheet('poison', 'poison.png', 32, 32);
+    game.load.spritesheet('star', 'star.png', 32, 32);
   }
 
   // initial game set up
